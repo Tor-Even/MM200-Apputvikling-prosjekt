7045b6036ff52302fcb172eb67f637cebf1f4c8b
FilipSzutkowski <filip99.ski@gmail.com>
2020-10-28 12:37:53 +0100

    Enkel post som lagrer elementer

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+app.post('/presentation', (req, res) => {
+    let presentation = {
+        elements: req.body,
+    }
+
+    res.status(200).json(presentation);
+    return
+})
 
 
 app.listen(app.get('port'), function () { console.log('server running', app.get('port'))});
